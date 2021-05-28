517bda8fad0f5ddc85163ee971631afa0bc46cc1
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2021-05-28 11:42:36 +0200

    Shortcut to config files

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as fs from "fs-extra";
+import * as path from "path";
 import * as vscode from "vscode";
 //import *  from './commands/vscode-sfdx-hardis.execute-command';
 import { HardisCommandsProvider } from "./hardis-commands-provider";
@@ -155,11 +157,58 @@ export function activate(context: vscode.ExtensionContext) {
       terminalStack.push(newTerminal);
     });
   });
+
   // Open external command
   vscode.commands.registerCommand("vscode-sfdx-hardis.openExternal", (url) =>
     vscode.env.openExternal(url)
   );
-  context.subscriptions.push(disposableTreeCommands);
+
+  // Open key file command
+  vscode.commands.registerCommand("vscode-sfdx-hardis.openKeyFile", async () => {
+    const keyFileList = [
+      { file: 'config/.sfdx-hardis.yml', label: 'sfdx-hardis main configuration file' },
+      { file: 'manifest/package.xml', label: 'List of all deployed metadatas' },
+      { file: 'manifest/packageDeployOnce.xml', label: 'List of metadatas that will be deployed only if not existing in the target org' },
+      { file: 'config/project-scratch-def.json', label: 'Scratch org creation definition file' },
+      { file: 'sfdx-project.json', label: 'Sfdx Project config file' },
+    ];
+    const quickpick = vscode.window.createQuickPick<vscode.QuickPickItem>();
+    const value = await new Promise<any>((resolve) => {
+      quickpick.ignoreFocusOut = true;
+      quickpick.title = "Please select a configuration file to open",
+        quickpick.canSelectMany = false;
+      quickpick.items = keyFileList
+        .filter((choice: any) => fs.existsSync(currentWorkspaceFolderUri + path.sep + choice.file))
+        .map((choice: any) => {
+          const quickPickItem: vscode.QuickPickItem = {
+            label: choice.file,
+            detail: choice.label
+          };
+          return quickPickItem;
+        });
+      quickpick.show();
+      quickpick.onDidHide(() => resolve(null));
+      quickpick.onDidAccept(() => {
+        if (quickpick.selectedItems.length > 0) {
+          const values = quickpick.selectedItems.map((item) => {
+            return keyFileList.filter((choice: any) => {
+              return item.label === choice.file;
+            })[0].file;
+          });
+          resolve(values);
+        }
+        resolve(null);
+      });
+    });
+    quickpick.dispose();
+    if (value) {
+      var openPath = vscode.Uri.parse("file:///" + currentWorkspaceFolderUri + '/' + value);
+      vscode.workspace.openTextDocument(openPath).then(doc => {
+        vscode.window.showTextDocument(doc);
+      });
+    }
+
+  });
 
   // Register Status TreeView
   const hardisStatusProvider = new HardisStatusProvider(
