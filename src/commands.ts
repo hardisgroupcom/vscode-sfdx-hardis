b7739fc8489ff50dfd312f9737e23480d8332896
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2025-08-29 19:04:37 +0200

    Improve Run local HTML doc command UX (#233)
    * Python detection & install link
    
    * Waiting message when starting doc
    
    * verbose !
    
    * [Mega-Linter] Apply linters fixes
    
    ---------
    
    Co-authored-by: nvuillam <17500430+nvuillam@users.noreply.github.com>

--- src/commands.ts
+++ src/commands.ts
@@ -6,7 +6,8 @@ import { HardisCommandsProvider } from "./hardis-commands-provider";
 import { HardisStatusProvider } from "./hardis-status-provider";
 import { HardisPluginsProvider } from "./hardis-plugins-provider";
 import { LocalWebSocketServer } from "./hardis-websocket-server";
-import { getWorkspaceRoot } from "./utils";
+import { getPythonCommand, getWorkspaceRoot } from "./utils";
+import axios from "axios";
 import TelemetryReporter from "@vscode/extension-telemetry";
 import { ThemeUtils } from "./themeUtils";
 import { exec } from "child_process";
@@ -66,6 +67,7 @@ export class Commands {
     this.registerShowExtensionConfig();
     this.registerShowPipelineConfig();
     this.registerShowInstalledPackages();
+    this.registerRunLocalHtmlDocPages();
   }
   registerShowExtensionConfig() {
     // Show the extensionConfig LWC panel for editing extension settings
@@ -545,6 +547,79 @@ export class Commands {
             });
           }
         });
+      },
+    );
+    this.disposables.push(disposable);
+  }
+
+  registerRunLocalHtmlDocPages() {
+    const disposable = vscode.commands.registerCommand(
+      "vscode-sfdx-hardis.runLocalHtmlDocPages",
+      async () => {
+        // Check how python is installed
+        const pythonCommand = await getPythonCommand();
+        if (!pythonCommand) {
+          vscode.window
+            .showErrorMessage(
+              "🦙 Python is not installed or not available in PATH. Please install Python to run the local documentation server.",
+              "Download and install Python",
+            )
+            .then((selection) => {
+              if (selection === "Download and install Python") {
+                vscode.env.openExternal(
+                  vscode.Uri.parse("https://www.python.org/downloads/"),
+                );
+              }
+            });
+          return;
+        }
+        const command = `${pythonCommand} -m pip install mkdocs-material mkdocs-exclude-search mdx_truly_sane_lists && mkdocs serve --verbose`;
+        vscode.commands.executeCommand(
+          "vscode-sfdx-hardis.execute-command",
+          command,
+        );
+        // Display a progress vscode UI message while the server starts (check that the server is started by pinging localhost:8000)
+        vscode.window.withProgress(
+          {
+            location: vscode.ProgressLocation.Notification,
+            title:
+              "Starting local documentation server...\n(it can take a while 😱)",
+            cancellable: true,
+          },
+          async (progress, token) => {
+            return new Promise<void>((resolve, reject) => {
+              let isResolved = false;
+              const interval = setInterval(() => {
+                axios
+                  .get("http://localhost:8000", { timeout: 2000 })
+                  .then(() => {
+                    if (!isResolved) {
+                      isResolved = true;
+                      clearInterval(interval);
+                      progress.report({
+                        message:
+                          "Local documentation server is running at http://localhost:8000",
+                      });
+                      vscode.env.openExternal(
+                        vscode.Uri.parse("http://localhost:8000"),
+                      );
+                      resolve();
+                    }
+                  })
+                  .catch(() => {
+                    // Server not started yet or not reachable
+                  });
+              }, 3000);
+              token.onCancellationRequested(() => {
+                if (!isResolved) {
+                  isResolved = true;
+                  clearInterval(interval);
+                  reject();
+                }
+              });
+            });
+          },
+        );
       },
     );
     this.disposables.push(disposable);
--- src/hardis-commands-provider.ts
+++ src/hardis-commands-provider.ts
@@ -842,8 +842,7 @@ export class HardisCommandsProvider
           {
             id: "hardis-run-doc",
             label: "Run Local HTML Doc Pages",
-            command:
-              "(pip install mkdocs-material mkdocs-exclude-search mdx_truly_sane_lists && mkdocs serve) || (python -m install mkdocs-material mkdocs-exclude-search mdx_truly_sane_lists && python -m mkdocs serve) || (py -m pip install mkdocs-material mkdocs-exclude-search mdx_truly_sane_lists && py -m mkdocs serve) || (pip install mkdocs-material mkdocs-exclude-search mdx_truly_sane_lists && python -m mkdocs serve -v)",
+            command: "vscode-sfdx-hardis.runLocalHtmlDocPages",
             tooltip:
               "Run Documentation local web server, then open http://127.0.0.1:8000/ . You need Python on your computer :)",
             requiresProject: true,
