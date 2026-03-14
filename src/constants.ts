2723bf385c55197629d3205a32974f6a344ff85a
Nicolas Vuillamy <nicolas.vuillamy@gmail.com>
2026-03-14 11:20:10 +0100

    No more alpha

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const RECOMMENDED_SFDX_CLI_VERSION = null; //"7.111.6";
 export const NODE_JS_MINIMUM_VERSION = 24.0;
 export const RECOMMENDED_MINIMAL_SFDX_HARDIS_VERSION: string = "6.7.1";
 export const BANNER_IMAGE_URL = showBanner
-  ? "https://raw.githubusercontent.com/hardisgroupcom/sfdx-hardis/refs/heads/alpha/docs/assets/images/cloudity-banner.png"
+  ? "https://raw.githubusercontent.com/hardisgroupcom/sfdx-hardis/refs/heads/main/docs/assets/images/cloudity-banner.png"
   : false;
 export const DOCSITE_URL = "https://sfdx-hardis.cloudity.com";
 export const WEBSITE_URL = "https://www.cloudity.com?ref=sfdxhardis";
